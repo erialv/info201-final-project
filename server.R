648f402e6f0ec52b8e2dcf9844eb69f0f635a703
Erianne Viray <erialv@uw.edu>
2017-03-04 16:55:42 -0800

    server additions

--- server.R
+++ server.R
@@ -10,12 +10,14 @@ server <- function(input, output) {
     average.score <- average.stem.data %>% 
       filter(Exam.Subject %in% input$subject) %>% 
       select(Exam.Subject, Score, Students..Male., Students..Female.) %>% 
-      filter(Score == input$score | Score == "All")
+      filter(Score == input$score | Score == "All") %>% 
+      mutate(male = Students..Male.[1], male.all = Students..Male.[2]) %>% 
+      mutate(female = Students..Female.[1], female.all = Students..Female.[2])
     
     percent <- average.score %>% 
       group_by(Exam.Subject) %>% 
-      summarize(Male.Percent = )
+      summarize(Male.Percent = male / male.all, Female.Percent = female / female.all) 
     
-    return()
+    return(percent)
   })
 }
