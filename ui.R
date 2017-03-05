05398227ed66994046ee353ea8afcae8bdbba07d
BenjaminTu <tu.benjamin1115@gmail.com>
2017-03-04 16:49:52 -0800

    graph ui

--- ui.R
+++ ui.R
@@ -10,7 +10,6 @@ average.stem.data <- data %>%
            Exam.Subject == "CALCULUS BC" | Exam.Subject == "COMPUTER SCIENCE A" | 
            Exam.Subject == "PHYSICS C: ELECTRICTY & MAGNETISM" | Exam.Subject == "PHYSICS C: MECHANICS" |
            Exam.Subject == "PHYSICS 1" | Exam.Subject == "PHYSICS 2" | Exam.Subject == "STATISTICS") %>% 
-  filter(Score == "Average" | Score == "All") %>% 
   select(-Students..11th.Grade., -Students..12th.Grade.)
 
 ui <- fluidPage(
@@ -18,7 +17,15 @@ ui <- fluidPage(
   navbarPage("Menu",
              tabPanel("Summary", verbatimTextOutput("summary")),
              tabPanel("Map", plotOutput("map")),
-             tabPanel("Bar Graph", plotOutput("box")),
+             tabPanel("Bar Graph", 
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectizeInput("subject", label = "Exam Subjects", 
+                                         choices = unique(average.stem.data$Exam.Subject),
+                                         multiple = TRUE), 
+                          sliderInput("score", label = "Score", min = 1, max = 5, value = 1)),
+                        mainPanel(
+                          plotOutput("box")))),
              tabPanel("Pie Chart", plotOutput("pie")),
              tabPanel("Analysis", textOutput("analysis")))
 )
